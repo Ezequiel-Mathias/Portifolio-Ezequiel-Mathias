70baa2a75bd157b3b7979f19779e5212552d7e61
Ezequiel Mathias <ezequielmathias32@gmail.com>
2023-06-09 12:55:05 -0300

    Effect Scroll reveal

---
+++ js/ScrollReveal.js
@@ -0,0 +1,35 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.short-presentation', {duration: 2000});
+
+sr.reveal('.about',  {
+    rotate: {x: 20, y: 80, z: 0},    
+    duration: 2000
+});
+
+sr.reveal('.profile-contacts',  {
+    rotate: {x: 20, y: 80, z: 20},    
+    duration: 2000
+});
+
+sr.reveal('.all-skills',  {
+    duration: 2000
+});
+
+sr.reveal('.container-card',  {
+    duration: 2000
+});
+
+sr.reveal('.container-means-of-contact',  {
+    duration: 2000
+});
+
+ sr.reveal('.contact-section p',  {
+    duration: 2000
+}); 
+
+sr.reveal('.developer-activity',  {
+    duration: 2000
+}); 
+
+
